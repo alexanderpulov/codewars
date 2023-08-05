de02523bed3e0f74cbf748c534724bda930bde5e
alexanderpulov <alec2918@gmail.com>
2023-08-05 13:38:33 +0300

    add two more tasks

---
+++ 8Kyu/calculate-average.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+
+
+// SOLUTION:
+function findAverage(array) {
+    if (array.length === 0) {
+      return 0
+    }
+    let sum = 0
+    for(i=0; i<array.length; i++) {
+      sum += array[i]
+    }
+    return sum / array.length
+  }
