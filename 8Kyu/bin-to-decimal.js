39b217d53a06d2dbcaf9141d97a28bfa2a933d80
alexanderpulov <alec2918@gmail.com>
2023-08-06 16:22:09 +0300

    parseint example

---
+++ 8Kyu/bin-to-decimal.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+
+// SOLUTION:
+function binToDec(bin){
+    let res = parseInt(bin, 2)
+    return res
+  }
