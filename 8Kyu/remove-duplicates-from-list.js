a88da231a7ecdb110bef34ed48962e59903930d7
alexanderpulov <alec2918@gmail.com>
2023-10-08 23:22:46 +0300

    remove-duplicates

---
+++ 8Kyu/remove-duplicates-from-list.js
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+    Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+    The order of the sequence has to stay the same.
+
+    Examples:
+
+Input -> Output
+    [1, 1, 2] -> [1, 2]
+    [1, 2, 1, 1, 3, 2] -> [1, 2, 3]*/
+
+
+
+
+//SOLUTION
+function distinct(a) {
+    const el = new Set(a)
+    const newArr = Array.from(el)
+    return newArr
+}
